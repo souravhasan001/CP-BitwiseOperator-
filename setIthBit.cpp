fd6b9d111536e6c6c4c5389f4ed50572b5211906
Sourav Hasan (Rafat) <97476707+souravhasan001@users.noreply.github.com>
2023-12-28 10:27:27 +0600

    Add files via upload

---
+++ setIthBit.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setIthBit(int &n,int i){
+
+	int mask= (1<<i);
+	n=(n|mask);
+
+}
+
+int main(){
+
+#ifndef ONLILE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+
+	int n=5;
+	int i;
+	cin>>i;
+
+	setIthBit(n,i);
+	cout<<n;
+}
